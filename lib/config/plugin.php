0d10559699b02e965a0af01d42b6ec1e0d5d7175
Serge Rodovnichenko <sergerod@gmail.com>
2022-11-12 04:29:53 +0300

    Autogenerated

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Сгорание бонусов',
+  'img' => 'img/burningbonus.gif',
+  'version' => '1.0.0',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopBurningbonusPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopBurningbonusPlugin extends shopPlugin
+{
+
+}
