3eba442f6fdc8d23a178ef878b0aa522dba02868
Serge Rodovnichenko <sergerod@gmail.com>
2022-11-23 01:31:24 +0300

    мета-файлы

---
+++ lib/config/requirements.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Serge Rodovnichenko <serge@syrnik.com>
+ * @version 1.0.0
+ * @copyright Serge Rodovnichenko, 2022
+ * @license http://www.webasyst.com/terms/#eula Webasyst
+ *
+ * Требования к системе, чтобы плагин работал
+ */
+return array(
+    'app.installer' => ['version' => '2.0', 'strict' => true],
+    'app.shop'      => ['version' => '8.18.0', 'strict' => true],
+    'php'           => ['version' => '>=7.4.0', 'strict' => true],
+);
