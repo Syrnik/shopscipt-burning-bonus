54489afa9a1e650030119a5101e1e38fae48670b
Serge Rodovnichenko <sergerod@gmail.com>
2022-11-23 19:04:47 +0300

    hotfix

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -8,7 +8,7 @@
 return [
     'name'          => 'Сгорание бонусов',
     'img'           => 'img/icon16.png',
-    'version'       => '1.0.0',
+    'version'       => '1.0.1',
     'vendor'        => '670917',
     'shop_settings' => true,
     'handlers'      => [
